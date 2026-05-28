d9d8a4424008b734a34d2b4be8bccbdbaf5b1382
Himani Nimma <it1298himani@gmail.com>
2026-05-28 16:26:55 +0530

    Add files via upload

---
+++ Overriding Polymorphism.py
@@ -0,0 +1,25 @@
+#overriding polymorphism
+'''class Animal:
+    def sound(self):
+        print("Animal makes sound")
+class Dog(Animal):
+    def sound(self):
+        print("Dog Barks")
+class Cat(Animal):
+    def sound(self):
+        print("Cat Meow")
+animals=[Animal(),Dog(),Cat()]
+for i in animals:
+    i.sound()'''
+class Food:
+    def biryani(self):
+        print("Ak biryani is famous in GNT")
+class Chicken(Food):
+    def biryani(self):
+        print("Chicken biryani")
+class Mutton(Food):
+    def biryani(self):
+        print("Mutton biryani")
+biryanis=[Chicken(),Mutton()]
+for i in biryanis:
+    i.biryani()
